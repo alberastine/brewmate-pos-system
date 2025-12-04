f8f6332177dc1bc0fcf79085f44a646663d3127f
alberastine <126144757+alberastine@users.noreply.github.com>
2025-12-04 20:30:00 +0800

    feat: added custom supply button

--- app/src/main/java/com/example/brewmate/activities/ManageSuppliesActivity.java
+++ app/src/main/java/com/example/brewmate/activities/ManageSuppliesActivity.java
@@ -1,9 +1,12 @@
 package com.example.brewmate.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,9 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.brewmate.R;
 
 public class ManageSuppliesActivity extends AppCompatActivity {
+
+    private LinearLayout addSupplyForm;
+    private Button btnCancel, btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,23 +37,54 @@ public class ManageSuppliesActivity extends AppCompatActivity {
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_left);
         }
 
+        addSupplyForm = findViewById(R.id.addSupplyForm);
+        btnCancel = findViewById(R.id.btnCancel);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_dashboard, menu);
-        for (int i = 0; i < menu.size(); i++) {
-            menu.getItem(i).setVisible(false);
+        MenuItem addItem = menu.findItem(R.id.action_add_supply);
+        if (addItem.getActionView() != null) {
+            addItem.getActionView().setOnClickListener(v -> toggleFormVisibility());
+        } else {
+            addItem.setOnMenuItemClickListener(item -> {
+                toggleFormVisibility();
+                return true;
+            });
         }
+
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem item = menu.getItem(i);
+            item.setVisible(item.getItemId() == R.id.action_add_supply);
+        }
+
         return true;
+    }
+
+    private void toggleFormVisibility() {
+        if (addSupplyForm.getVisibility() == View.GONE) {
+            addSupplyForm.setVisibility(View.VISIBLE);
+            btnSubmit.setText(R.string.add_product_sub_btn_label);
+        } else {
+            addSupplyForm.setVisibility(View.GONE);
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            Intent intent = new Intent(this, AdminDashboardActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+            finish();
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
